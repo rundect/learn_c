1b470e97d1207dd3994c953b47b8770030f2a721
Роман Афанасьев <roman.afanasev@innostage-group.ru>
2023-05-29 22:49:55 +0300

    Init

---
+++ 1_1_intro/hello_neo.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void) {
+  printf("Wake up, Neo...\n\nThe Matrix has you...\n\nFollow the white rabbit...");
+  return (0);
+}
---
+++ 1_1_intro/neo_data.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void) {
+  printf("NICKNAME: Neo\nCITY: St-Petersburg\nAGE: 35\nHEIGHT: 180\nWEIGHT: 75");
+  return (0);
+}
+
