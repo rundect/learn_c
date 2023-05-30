462930cc7837fc4c49ed07c9eccaf0191bbcf245
Роман Афанасьев <roman.afanasev@innostage-group.ru>
2023-05-30 23:55:57 +0300

    Add printf

---
+++ 3_1_printf/sherlock.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+int main(void){
+    char name = "S.Holmes:";
+    char latitude = "51grad 31\'25.48\" N";
+    char longitude = "0 grad  9\'29.93\" W";
+
+    printf("%c\n%c\n%c\n", name, latitude, longitude);
+}
