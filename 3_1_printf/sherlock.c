870ff776e25c99bc2596a3d98fa72eb5ae467b38
Роман Афанасьев <roman.afanasev@innostage-group.ru>
2023-05-31 23:08:24 +0300

    Complete stage 3_1

--- 3_1_printf/sherlock.c
+++ 3_1_printf/sherlock.c
@@ -1,8 +1,10 @@
 #include <stdio.h> 
 int main(void){
-    char name = "S.Holmes:";
-    char latitude = "51grad 31\'25.48\" N";
-    char longitude = "0 grad  9\'29.93\" W";
+    char name[] = "S.Holmes:";
+    char latitude[] = "51grad 31\'25.48\" N";
+    char longitude[] = "0 grad  9\'29.93\" W";
 
-    printf("%c\n%c\n%c\n", name, latitude, longitude);
+    printf("%s\n%s\n%s\n", name, latitude, longitude);
+
+    return 0 ;
 }
